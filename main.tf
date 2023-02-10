27cf1005eaa4d5035e5ffbe8e1f3cfa1e86ba889
abhi <abhi@gmail.com>
2023-02-10 14:45:35 +0530

    4th commit

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "example" {
   name     = var.name
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "example1" {
+  name     = "tfcloudrg"
+  location = "eastus"
+}
